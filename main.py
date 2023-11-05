6f0b6ad841ce3e22f0a546a37b0995e616750b53
Zigrippa <alter0028@gmail.com>
2023-11-05 18:35:01 +0600

    lesson 2, update

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import telebot
+import webbrowser
 
 bot = telebot.TeleBot('5842283991:AAHFRRqJYtub0NyLXIxhftpOcxw1zFpa4dA')
 
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-    elif message.text == "/task":
-        pass
-    else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
-
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'Приветствую тебя!')
+    bot.send_message(message.chat.id, f'<em>Приветствую тебя, {message.from_user.first_name}!</em>', parse_mode='html')
+
+@bot.message_handler(commands=['help'])
+def start(message):
+    bot.send_message(message.chat.id, 'Список доступных комманд:\n'
+                                      '/start\n'
+                                      '/help\n')
+
+#@bot.message_handler(commands=['site', 'website'])
+#def start(message):
+#    webbrowser.open('https://google.com')
+
+@bot.message_handler()
+def info(message):
+    if message.text.lower() == "привет":
+        bot.send_message(message.chat.id, "Привет, чем я могу тебе помочь?")
+    elif message.text.lower() == "id":
+        bot.reply_to(message, f'ID:{message.from_user.id}')
 
 
 bot.polling(none_stop=True, interval=0)
