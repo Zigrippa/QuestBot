362942748660c57366f3682898e255708afd6e7f
Zigrippa <alter0028@gmail.com>
2023-11-07 23:57:28 +0600

    lesson 3, update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import telebot
 import webbrowser
+from telebot import types
 
 bot = telebot.TeleBot('5842283991:AAHFRRqJYtub0NyLXIxhftpOcxw1zFpa4dA')
 
@@ -23,6 +24,13 @@ def info(message):
         bot.send_message(message.chat.id, "Привет, чем я могу тебе помочь?")
     elif message.text.lower() == "id":
         bot.reply_to(message, f'ID:{message.from_user.id}')
+
+@bot.message_handler(content_types=['photo'])
+def get_photo(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton('Перейти на сайт', url='https://google.com'))
+    markup.add(types.InlineKeyboardButton('Удалить фото', callback_data='delete'))
+    bot.reply_to(message, 'Какое красиво фото!', reply_markup=markup)
 
 
 bot.polling(none_stop=True, interval=0)
